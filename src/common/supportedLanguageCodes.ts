72a000d6364134656ab50e7da356b64e98f7c97d
borohhov <dmitri@ausalt.com>
2025-09-21 12:55:12 +0300

    added CZ

--- src/common/supportedLanguageCodes.ts
+++ src/common/supportedLanguageCodes.ts
@@ -1,2 +1,2 @@
 export type SourceLangCode = 'en-US' | 'es-US' | 'fr-FR';
-export type TargetLangCode = 'en-US' | 'ru-RU' | 'et-EE' | 'lv-LV' | 'lt-LT' | 'sr-RS';
+export type TargetLangCode = 'en-US' | 'ru-RU' | 'et-EE' | 'lv-LV' | 'lt-LT' | 'sr-RS' | 'cs-CZ';
